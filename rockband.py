031edff429a5b833a45b52c755f13df87efcf487
Ben Hocker <ben.hocker@cat.com>
2021-04-29 23:41:01 -0500

    Adds yaml and logging support

--- rockband.py
+++ rockband.py
@@ -1,12 +1,46 @@
 #!/usr/bin/python
 
+# Std Python Libs
+import logging
+import sys
+
+# 3rd Party Libs
 from helium import *
+import yaml
 
-song = 'One Headlight'
-artist = 'The Wallflowers'
+def load_work(config_file = './config.yaml'):
+    song_list = []
 
-start_chrome('www.harmonixmusic.com/games/rock-band/request')
-write(song, into='Song Title')
-write(artist, into='Artist')
-click('Submit')
-kill_browser()
+    logging.info(f"Loading {config_file}")
+    with open(config_file, 'r') as f:
+        return yaml.load(f)['songs']
+
+def submit_request(song, artist, url='www.harmonixmusic.com/games/rock-band/request'):
+    logging.info(f"Logging into {url}")
+    start_chrome(url)
+    write(song, into='Song Title')
+    write(artist, into='Artist')
+    logging.info(f"Submitting request for '{song}' by '{artist}'")
+    click('Submit')
+    kill_browser()
+
+def main():
+    # Set up logging
+    logging.basicConfig(
+        format="%(asctime)-15s %(levelname)s %(message)s", level='INFO'
+    )
+
+    logging.info('Entering main')
+    song_list = load_work()
+    for song_info in song_list:
+        logging.debug(f"Song: {song_info['song']}, Artist: {song_info['artist']}")
+        submit_request(
+            song_info['song'],
+            song_info['artist']
+        )
+
+    # Stop logging and clean up
+    logging.shutdown()
+
+if __name__ == "__main__":
+    sys.exit(main())
