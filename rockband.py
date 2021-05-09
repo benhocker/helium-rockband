29e35d58cde034a771dcf7ba8466279b02e7a798
Ben Hocker <benhocker@users.noreply.github.com>
2021-05-08 19:11:43 -0500

    Update python-app.yml (#3)
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Fixes python lint
    
    * Fixes all pylint errors
    
    * Adds pipenv check
    
    * Adds more CI testing and code fixes
    
    * Adds dev packages to CI pipenv
    
    * Fixes last pylint error
    
    * Calling out yaml file directly to make test work
    
    * removes variable from logging

--- rockband.py
+++ rockband.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python
 
+"This script logs into the Rock Band website and requests new songs"
+
 # Std Python Libs
 import logging
 import sys
 
 # 3rd Party Libs
-from helium import *
+from helium import start_chrome, write, click, kill_browser
 import yaml
 
-def load_work(config_file = './config.yaml'):
-    song_list = []
 
-    logging.info(f"Loading {config_file}")
-    with open(config_file, 'r') as f:
-        return yaml.load(f)['songs']
+def load_work(config_file='./config.yaml'):
+    "Loads yaml config file"
 
-def submit_request(song, artist, url='www.harmonixmusic.com/games/rock-band/request'):
-    logging.info(f"Logging into {url}")
+    logging.info("Loading yaml file")
+    with open(config_file, 'r') as file_reader:
+        return yaml.load(file_reader)['songs']
+
+
+def submit_request(
+                    song,
+                    artist,
+                    url='www.harmonixmusic.com/games/rock-band/request'
+                  ):
+    "Uses helium framework to open chrome, open a website, and submit a song"
+
+    logging.info("Logging into %s", url)
     start_chrome(url, headless=True)
     write(song, into='Song Title')
     write(artist, into='Artist')
-    logging.info(f"Submitting request for '{song}' by '{artist}'")
+    logging.info("Submitting request for '%s' by '%s'", song, artist)
     click('Submit')
     kill_browser()
 
+
 def main():
+    "This is the main method"
+
     # Set up logging
     logging.basicConfig(
         format="%(asctime)-15s %(levelname)s %(message)s", level='INFO'
@@ -33,7 +46,9 @@ def main():
     logging.info('Entering main')
     song_list = load_work()
     for song_info in song_list:
-        logging.debug(f"Song: {song_info['song']}, Artist: {song_info['artist']}")
+        logging.debug("Song: %s, Artist: %s",
+                      song_info['song'],
+                      song_info['artist'])
         submit_request(
             song_info['song'],
             song_info['artist']
@@ -41,6 +56,7 @@ def main():
 
     # Stop logging and clean up
     logging.shutdown()
+
 
 if __name__ == "__main__":
     sys.exit(main())
